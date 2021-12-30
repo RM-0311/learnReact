5b068a4f88e6c51f52313682a7f39e50f365dc69
unknown <md.ridgeway.work@gmail.com>
2021-12-30 20:57:14 +0100

    React Fragments

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css'
-import { useState } from 'react'
+import React, { useState } from 'react'
 import Title from './components/Title'
 
 
@@ -28,7 +28,7 @@ function App() {
     <div className="App">
       <Title title="Events in Your Area" subtitle={subtitle}/>
       <Title title="another title" subtitle="another subtitle"/>
-      
+
       {showEvents && (
         <div>
           <button onClick={() => setShowEvents(false)}>hide events</button>
@@ -40,10 +40,10 @@ function App() {
         </div>
       )}
       {showEvents && events.map((event, index) => (
-        <div key={event.id}>
+        <React.Fragment key={event.id}>
           <h2>{index} - {event.title}</h2>
           <button onClick={() => handleClick(event.id)}>delete event</button>
-        </div>
+        </React.Fragment>
       ))}
     </div>
   );
