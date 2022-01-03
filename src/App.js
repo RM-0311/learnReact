743c4acaec825582226dc186d03bf6361162572d
unknown <md.ridgeway.work@gmail.com>
2022-01-03 14:38:49 +0100

    Reusable Event Lists

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css'
-import React, { useState } from 'react'
+import { useState } from 'react'
 import Title from './components/Title'
 import Modal from './components/Modal'
+import EventList from './components/EventList'
 
 
 function App() {
@@ -44,12 +45,7 @@ function App() {
           <button onClick={() => setShowEvents(true)}>show events</button>
         </div>
       )}
-      {showEvents && events.map((event, index) => (
-        <React.Fragment key={event.id}>
-          <h2>{index} - {event.title}</h2>
-          <button onClick={() => handleClick(event.id)}>delete event</button>
-        </React.Fragment>
-      ))}
+      {showEvents && <EventList events={events} handleClick={handleClick}/>}
 
       {showModal && <Modal handleClose={handleClose}>
         <h2>Terms and Conditions</h2>
