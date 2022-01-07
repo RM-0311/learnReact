716b91d19dc56b7d1edd10836ed4473f322d025b
unknown <md.ridgeway.work@gmail.com>
2022-01-07 14:18:03 +0100

    Controlled Inputs and onSubmit

--- src/components/NewEventForm.js
+++ src/components/NewEventForm.js
@@ -5,22 +5,42 @@ export default function NewEventForm() {
   const [title, setTitle] = useState('')
   const [date, setDate] = useState('')
 
-  // const handleChange = (e) => {
-  //   setTitle(e.target.value)
-  // }
+  const resetForm = () => {
+    setTitle('')
+    setDate('')
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    const event = {
+      title: title,
+      date: date,
+      id: Math.floor(Math.random() * 10000)
+    }
+    console.log(event)
+    resetForm()
+  }
 
   return (
-    <form className='new-event-form'>
+    <form className="new-event-form" onSubmit={handleSubmit}>
       <label>
         <span>Event Title:</span>
-        <input type="text" onChange={(e) => setTitle(e.target.value)}/>
+        <input 
+          type="text" 
+          onChange={(e) => setTitle(e.target.value)} 
+          value={title} 
+        />
       </label>
       <label>
         <span>Event Date:</span>
-        <input type="date" onChange={(e) => setDate(e.target.value)} />
+        <input 
+          type="date" 
+          onChange={(e) => setDate(e.target.value)} 
+          value={date} 
+        />
       </label>
       <button>Submit</button>
-      <p>title - {title}, date - {date}</p>
     </form>
   )
 }
